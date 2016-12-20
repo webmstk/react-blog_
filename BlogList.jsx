55da9305790cb32b0918f541e7348d149924423a
webmstk <webmstk@mail.ru>
2016-12-20 11:24:02 +0300

    lesson1 remarks

--- BlogList.jsx
+++ BlogList.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import BlogItem from './BlogItem'
 
 const BlogList = ({ posts }) => {
-  const blogItems = posts.map((obj, key) => {
-    obj.key = key;
-    return React.createElement(BlogItem, obj);
-  });
+  const blogItems = posts.map((obj, key) =>
+    React.createElement(BlogItem, Object.assign(obj, { key: key }))
+  );
 
   return React.createElement('div', null, blogItems);
 };
